f74105ff2c088a5d72477f4027f4f31097ef7b04
Jazastry <you@example.com>
2017-12-22 16:00:55 +0200

    Adds return ip to the caller.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
   var ip = req.headers['x-real-ip'] || req.connection.remoteAddress || req.ip
   console.log('ip', ip)
+  res.send(`IP : ${ip}`)
 })
 
 app.listen(8888, () => console.log('Example app listening on port 8888!'))
